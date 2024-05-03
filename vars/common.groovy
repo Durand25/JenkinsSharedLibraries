04c845d3fc373c05bde4050f0437518f19815c6c
Durand25 <152901817+Durand25@users.noreply.github.com>
2024-05-03 22:35:31 +0200

    Update common.groovy

--- vars/common.groovy
+++ vars/common.groovy
@@ -2,18 +2,21 @@ def call(String stageName){
   
   if ("${stageName}" == "Build")
      {
-       sh "mvn clean package"
+       //sh "mvn clean package"
+       sh "${mavenHome}/bin/mvn package"
        sh "echo 'running JUnit-test-cases' "
       sh "echo 'testing must passed to create artifacts ' "
      }
   else if ("${stageName}" == "SonarQube Report")
      {
-       sh "mvn clean sonar:sonar"
+       //sh "mvn clean sonar:sonar"
+       sh "${mavenHome}/bin/mvn sonar:sonar"
        sh "echo CodeQualityAnalysis completed"
      }
   else if ("${stageName}" == "Upload Into Nexus")
      {
-       sh "mvn clean deploy"
+       //sh "mvn clean deploy"
+       sh "${mavenHome}/bin/mvn deploy"
        sh "echo artifacts uploaded successfully"
           sh "echo I am now a Build and Release Engineer"
           sh "echo Build and release completed"
